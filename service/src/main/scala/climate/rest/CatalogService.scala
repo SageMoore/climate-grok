933eba2de3453b93db8cdfaf0848f6ef95862d47
Eugene Cheipesh <echeipesh@gmail.com>
2014-12-15 14:11:51 -0500

    factoring out zonal summary to it's own route

--- service/src/main/scala/climate/rest/CatalogService.scala
+++ service/src/main/scala/climate/rest/CatalogService.scala
@@ -1,9 +1,9 @@
 package climate.rest
 
-
 import akka.actor.ActorSystem
+import climate.op._
+import com.github.nscala_time.time.Imports._
 import com.quantifind.sumac.{ ArgApp, ArgMain }
-
 import geotrellis.proj4._
 import geotrellis.raster._
 import geotrellis.raster.io.geotiff.Nodata
@@ -11,48 +11,35 @@ import geotrellis.raster.render._
 import geotrellis.spark._
 import geotrellis.spark.cmd.args._
 import geotrellis.spark.io.accumulo._
+import geotrellis.spark.io.hadoop._
 import geotrellis.spark.json._
 import geotrellis.spark.tiling._
 import geotrellis.spark.utils.SparkUtils
+import geotrellis.vector._
 import geotrellis.vector.reproject._
-
 import org.apache.accumulo.core.client.security.tokens.PasswordToken
-import org.joda.time.DateTime
 import scala.concurrent._
 import scala.concurrent.ExecutionContext.Implicits.global
-
-import spray.http.HttpHeaders._
-import spray.http.HttpMethods._
-import spray.http.{ AllOrigins, MediaTypes }
-import spray.http.{ HttpMethods, HttpMethod, HttpResponse, AllOrigins }
+import spray.http.{MediaTypes }
 import spray.httpx.SprayJsonSupport
 import spray.json._
 import spray.routing._
-import spray.routing.SimpleRoutingApp
-import climate.op._
 
-trait CORSSupport { self: HttpService =>
-  val corsHeaders = List(`Access-Control-Allow-Origin`(AllOrigins),
-    `Access-Control-Allow-Methods`(GET, POST, OPTIONS, DELETE),
-    `Access-Control-Allow-Headers`("Origin, X-Requested-With, Content-Type, Accept, Accept-Encoding, Accept-Language, Host, Referer, User-Agent"))
 
-  def cors: Directive0 = {
-    val rh = implicitly[RejectionHandler]
-    respondWithHeaders(corsHeaders) & handleRejections(rh)
-  }
-}
+class CatalogArgs extends AccumuloArgs
 
 /**
  * Catalog and TMS service for TimeRaster layers only
  * This is intended to exercise the machinery more than being a serious service.
  */
-object CatalogService extends ArgApp[AccumuloArgs] with SimpleRoutingApp with SprayJsonSupport with CORSSupport {
+object CatalogService extends ArgApp[CatalogArgs] with SimpleRoutingApp with SprayJsonSupport with CorsSupport with ZonalSummaryRoutes {
   implicit val system = ActorSystem("spray-system")
   implicit val sparkContext = SparkUtils.createSparkContext("Catalog")
 
   val accumulo = AccumuloInstance(argHolder.instance, argHolder.zookeeper,
     argHolder.user, new PasswordToken(argHolder.password))
   val catalog = accumulo.catalog
+  //val catalog: HadoopCatalog = HadoopCatalog(sparkContext, new Path("hdfs://localhost/catalog"))
 
   /** Simple route to test responsiveness of service. */
   val pingPong = path("ping")(complete("pong"))
@@ -100,16 +87,13 @@ object CatalogService extends ArgApp[AccumuloArgs] with SimpleRoutingApp with Sp
         complete {
           import DefaultJsonProtocol._
 
-          accumulo.metaDataCatalog.fetchAll.mapValues(_._1).toSeq.map {
-            case (layer, md) =>                          
+          accumulo.metaDataCatalog.fetchAll.toSeq.map {
+            case (key, lmd) =>
+              val (layer, table) = key
+              val md = lmd.rasterMetaData
               val center = md.extent.reproject(md.crs, LatLng).center
-              val breaks = {
-                (if (layer.name == "NLCD")
-                  Histogram(catalog.load[SpatialKey](layer).get)
-                else
-                  Histogram(catalog.load[SpaceTimeKey](layer).get)
-                ).getQuantileBreaks(12)
-              }
+              val breaks = lmd.histogram.get.getQuantileBreaks(12)
+
               JsObject(
                 "layer" -> layer.toJson,
                 "metaData" -> md.toJson,
@@ -122,8 +106,8 @@ object CatalogService extends ArgApp[AccumuloArgs] with SimpleRoutingApp with Sp
     } ~ 
     pathPrefix(Segment / IntNumber) { (name, zoom) =>      
       val layer = LayerId(name, zoom)
-      val (md, params) = accumulo.metaDataCatalog.load(layer).get
-
+      val (lmd, params) = catalog.metaDataCatalog.load(layer).get
+      val md = lmd.rasterMetaData
       (path("bands") & get) { 
         import DefaultJsonProtocol._
         complete{ future {          
@@ -152,12 +136,96 @@ object CatalogService extends ArgApp[AccumuloArgs] with SimpleRoutingApp with Sp
       }
     }
   }
-  def root = {
-    pathPrefix("catalog") { catalogRoute } ~
-      pathPrefix("tms") { tmsRoute }
+
+
+  // import scalaz._
+
+  // def getLayer = Memo.mutableHashMapMemo{ layer: LayerId =>
+  //   val rdd = catalog.load[SpaceTimeKey](layer).get
+  //   asRasterRDD(rdd.metaData) {
+  //     rdd.repartition(8).cache
+  //   }
+  // }
+
+
+
+  
+  def timedCreate[T](startMsg:String,endMsg:String)(f:() => T):T = {
+    println(startMsg)
+    val s = System.currentTimeMillis
+    val result = f()
+    val e = System.currentTimeMillis
+    val t = "%,d".format(e-s)
+    println(s"\t$endMsg (in $t ms)")
+    result
   }
 
-  startServer(interface = "localhost", port = 8080) {
-    get(pingPong ~ root)
+  def pixelRoute = cors {
+    import DefaultJsonProtocol._
+    import org.apache.spark.SparkContext._
+
+    path("pixel") {
+      get {
+        parameters('name, 'zoom.as[Int], 'x.as[Double], 'y.as[Double]) { (name, zoom, x, y) =>
+          val layer = LayerId(name, zoom)
+          val (lmd, params) = catalog.metaDataCatalog.load(layer).get
+          val md = lmd.rasterMetaData
+          val crs = md.crs
+
+          val p = Point(x, y).reproject(LatLng, crs)
+          val key = md.mapTransform(p)
+          val rdd = catalog.load[SpaceTimeKey](layer, FilterSet(SpaceFilter[SpaceTimeKey](key.col, key.row))).get
+          val bcMetaData = rdd.sparkContext.broadcast(rdd.metaData)
+
+          def createCombiner(value: Double): (Double, Double) =
+            (value, 1)
+          def mergeValue(acc: (Double, Double), value: Double): (Double, Double) =
+            (acc._1 + value, acc._2 + 1)
+          def mergeCombiners(acc1: (Double, Double), acc2: (Double, Double)): (Double, Double) =
+            (acc1._1 + acc2._1, acc1._2 + acc2._2)
+
+          complete {
+            val data: Array[(DateTime, Double)] =
+            timedCreate("Starting calculation", "End calculation") {
+              rdd
+                .mapKeys { key => key.updateTemporalComponent(key.temporalKey.time.withMonthOfYear(1).withDayOfMonth(1).withHourOfDay(0)) }
+                .map { case (key, tile) =>
+                  val md = bcMetaData.value
+                  val (col, row) = RasterExtent(md.mapTransform(key), tile.cols, tile.rows).mapToGrid(p.x, p.y)
+                  (key, tile.getDouble(col, row))
+                 }
+                .combineByKey(createCombiner, mergeValue, mergeCombiners)
+                .map { case (key, (sum, count)) =>
+                  (key.temporalKey.time, sum / count)
+                 }
+                .collect
+            }
+
+            JsArray(JsObject(
+              "model" -> JsString(name),
+              "data" -> JsArray(
+                data.map { case (year, value) =>
+                  JsObject(
+                    "year" -> JsString(year.toString),
+                    "value" -> JsNumber(value)
+                  )
+                }: _*
+              )
+            ))
+          }
+        }
+      }
+    }
+  }
+
+  def root = {
+    pathPrefix("catalog") { catalogRoute } ~
+      pathPrefix("tms") { tmsRoute } ~
+      pathPrefix("stats") { zonalRoutes(catalog) } ~
+      pixelRoute
+  }
+
+  startServer(interface = "0.0.0.0", port = 8088) {
+    pingPong ~ root
   }
 }
